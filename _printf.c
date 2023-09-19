4e228a0b611180d50ec96973fa74dbdad3db62e5
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-09-18 19:40:57 -0700

    Resolve trailing white space error in betty

--- _printf.c
+++ _printf.c
@@ -8,25 +8,24 @@
 int _printf(const char *format, ...)
 {
 	unsigned int i, count;
+
 	va_list params;
+
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	va_start(params, format);
+va_start(params, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-
 		if (format[i] != '%')
 		{
 			count += output_char(format[i]);
 		}
 		else
 		{
-			count += print_format(format[i + 1], params);
-			i++;	
+		count += print_format(format[i + 1], params);
+		i++;
 		}
-
 	}
 	va_end(params);
 	return (count);
 }
-
